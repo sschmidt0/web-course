c06b92d27c0857ce098c8867485dac621928d042
sschmidt0 <sschmidt0@uoc.edu>
2025-02-22 10:21:28 +0100

    feat: add basic test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      exclude: ["**/index.ts"],
+      provider: "v8", // or 'v8'
+      reporter: ["text", "json", "html"],
+    },
+    setupFiles: "./test-setup.ts",
+    include: ["./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "./src/config/**",
+      "**/index.ts",
+    ],
+  },
+});

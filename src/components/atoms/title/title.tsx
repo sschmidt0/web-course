c3b7c7a4634b87c3321a859a9a9c4fda77e23f14
sschmidt0 <sschmidt0@uoc.edu>
2025-03-01 13:40:50 +0100

    feat: add styles to title

--- src/components/atoms/title/title.tsx
+++ src/components/atoms/title/title.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./title.module.scss";
 
 export interface TitleProps {
   text: string;
 }
 
 export const Title: React.FC<TitleProps> = ({ text }) => {
-  return <h1>{text}</h1>;
+  return <h1 className={styles.title}>{text}</h1>;
 };

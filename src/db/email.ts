01c432ebd768b06ea1da5c3a7e05839472d31fba
sschmidt0 <sschmidt0@uoc.edu>
2025-03-04 22:15:11 +0100

    feat: improve email-template

--- src/db/email.ts
+++ src/db/email.ts
@@ -1,11 +1,32 @@
 export const EMAIL_MESSAGE = {
   english: {
     subject: "No reply: Your message has been delivered",
+    title: "Web Development Course",
+    greeting: "Hello",
+    thanksText:
+      "Thank you for contacting me. I have received your message and will get back to you as soon as possible.",
+    userMessage: "Your message:",
+    userEmail: "Your email:",
+    thanks: "Thank you",
   },
   spanish: {
     subject: "No responder: Tu mensaje ha sido entregado",
+    title: "Curso de Desarrollo Web",
+    greeting: "Hola",
+    thanksText:
+      "Gracias por ponerte en contacto conmigo. He recibido tu mensaje y te responderé lo antes posible.",
+    userMessage: "Tu mensaje:",
+    userEmail: "Tu correo electrónico:",
+    thanks: "Gracias",
   },
   catalan: {
     subject: "No responguis: El teu missatge ha estat entregat",
+    title: "Curs de Desenvolupament Web",
+    greeting: "Hola",
+    thanksText:
+      "Gràcies per posar-te en contacte amb mi. He rebut el teu missatge i et respondré tan aviat com sigui possible.",
+    userMessage: "El teu missatge:",
+    userEmail: "El teu correu electrònic:",
+    thanks: "Gràcies",
   },
 };
--- src/emails/send-confirmation.tsx
+++ src/emails/send-confirmation.tsx
@@ -10,43 +10,61 @@ import {
   Section,
   Text,
 } from "@react-email/components";
+import { EMAIL_MESSAGE } from "@/db/email";
+import { Language } from "@/common/model/language";
 
 export interface SendConfirmationProps {
   email: string;
   message: string;
   username: string;
+  language: Language;
 }
 
 const SendConfirmation: React.FC<Readonly<SendConfirmationProps>> = ({
-  username: userName,
-  message,
-  email,
+  username = "Sarah Schmidt",
+  message = "Lorem ipsum",
+  email = "sakschmidt@gmail.com",
+  language = "catalan",
 }) => {
-  const previewText = `Read ${userName}'s review - ${email}`;
+  const subject = EMAIL_MESSAGE[language]?.subject;
+  const title = EMAIL_MESSAGE[language]?.title;
+  const greeting = EMAIL_MESSAGE[language]?.greeting;
+  const thanksText = EMAIL_MESSAGE[language]?.thanksText;
+  const userMessage = EMAIL_MESSAGE[language]?.userMessage;
+  const userEmail = EMAIL_MESSAGE[language]?.userEmail;
+  const thanks = EMAIL_MESSAGE[language]?.thanks;
 
   return (
     <Html>
       <Head />
 
       <Body style={main}>
-        <Preview>{previewText}</Preview>
+        <Preview>{subject}</Preview>
         <Container style={container}>
           <Section style={{ paddingBottom: "20px" }}>
             <Row>
-              <Text style={heading}>Here's what {userName} wrote</Text>
+              <Text style={{ ...heading, paddingBottom: "16px" }}>
+                www.sarahschmidt.es - {title}
+              </Text>
+              <Text style={paragraph}>
+                {greeting} {username} 👋🏻
+              </Text>
+              <Text style={paragraph}>{thanksText}</Text>
+              <Hr style={hr} />
+              <Text style={paragraph}>{userMessage}</Text>
               <Text style={review}>{message}</Text>
               <Text style={paragraph}>
-                Now that the review period is over, we’ve posted {userName}
-                ’s review to your Airbnb profile.
+                {userEmail} {email}
               </Text>
-              <Text style={{ ...paragraph, paddingBottom: "16px" }}>
-                While it’s too late to write a review of your own, you can send
-                your feedback to {userName} using your Airbnb message thread.
+              <Hr style={hr} />
+              <Text style={{ ...paragraph, paddingBottom: "8px" }}>
+                {thanks},
+              </Text>
+              <Text style={{ ...signature, paddingBottom: "16px" }}>
+                Sarah Schmidt
               </Text>
             </Row>
           </Section>
-
-          <Hr style={hr} />
         </Container>
       </Body>
     </Html>
@@ -84,11 +102,16 @@ const paragraph = {
 const review = {
   ...paragraph,
   padding: "24px",
-  backgroundColor: "#f2f3f3",
+  backgroundColor: "#f0edff",
   borderRadius: "4px",
 };
 
 const hr = {
-  borderColor: "#cccccc",
+  borderColor: "#634aff",
   margin: "20px 0",
+};
+
+const signature = {
+  fontFamily: '"Comic Sans MS", "Comic Sans", cursive',
+  fontSize: "20px",
 };

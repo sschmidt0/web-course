eeb34d86f7899ab7a94be004311740db6797cd4c
sschmidt0 <sschmidt0@uoc.edu>
2025-03-11 22:01:47 +0100

    feat: delete social media icons from array-text and move to about component

--- src/components/molecules/array-text/array-text.tsx
+++ src/components/molecules/array-text/array-text.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import cx from "classnames";
 import styles from "./array-text.module.scss";
-import { APP_ICONS } from "@/common/app-icons";
 
 export interface ArrayTextProps {
   className?: string;
+  children?: React.ReactNode;
   textos: { id: number; text: string }[];
   withBold?: boolean;
 }
 
 export const ArrayText: React.FC<ArrayTextProps> = ({
   className,
+  children,
   textos,
   withBold,
 }) => {
-  const linkedinIcon = APP_ICONS["linkedin" as keyof typeof APP_ICONS];
-  const githubIcon = APP_ICONS["github" as keyof typeof APP_ICONS];
-
   return (
     <div className={cx(styles.container, className)}>
       {textos.map((text) => {
@@ -27,22 +25,7 @@ export const ArrayText: React.FC<ArrayTextProps> = ({
           </p>
         );
       })}
-      <div className={styles["icon-container"]}>
-        <a
-          href="https://www.linkedin.com/in/sschmidt0/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className={styles.icon}>{linkedinIcon}</div>
-        </a>
-        <a
-          href="https://github.com/sschmidt0/sschmidt0"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className={styles.icon}>{githubIcon}</div>
-        </a>
-      </div>
+      {children}
     </div>
   );
 };
--- src/pods/about/about.component.tsx
+++ src/pods/about/about.component.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { ArrayText, InfoBoxList, Title, UnderlineList } from "@/components";
 import { PAGE_ABOUT } from "@/db/about";
 import { useLanguageStore } from "@/store";
+import { ArrayText, InfoBoxList, Title, UnderlineList } from "@/components";
 import { Picture } from "./components/picture";
+import { APP_ICONS } from "@/common/app-icons";
 import styles from "./about.module.scss";
 
 export const About: React.FC = () => {
@@ -19,12 +20,32 @@ export const About: React.FC = () => {
   const fourthBlockItems = content.fourthBlock.items;
   const fourthBlockIcon = content.fourthBlock.icon;
 
+  const linkedinIcon = APP_ICONS["linkedin" as keyof typeof APP_ICONS];
+  const githubIcon = APP_ICONS["github" as keyof typeof APP_ICONS];
+
   return (
     <div className={styles.container}>
       <Title text={title} />
       <div className={styles["intro-container"]}>
         <Picture />
-        <ArrayText className={styles["author-text"]} textos={firstBlockItems} />
+        <ArrayText className={styles["author-text"]} textos={firstBlockItems}>
+          <div className={styles["icon-container"]}>
+            <a
+              href="https://www.linkedin.com/in/sschmidt0/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className={styles.icon}>{linkedinIcon}</div>
+            </a>
+            <a
+              href="https://github.com/sschmidt0/sschmidt0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className={styles.icon}>{githubIcon}</div>
+            </a>
+          </div>
+        </ArrayText>
       </div>
       <InfoBoxList
         itemStyles={styles.items}

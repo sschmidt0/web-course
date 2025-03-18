96b189687cad18fb5ddff3deef34d34ccd80eed7
sschmidt0 <sschmidt0@uoc.edu>
2025-03-18 22:37:23 +0100

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { HeaderContainer } from "@/pods/header";
 import styles from "./layout.module.scss";
 import "./global.styles.scss";
@@ -15,6 +16,7 @@ export default function RootLayout({
           <HeaderContainer />
           {children}
         </div>
+        <SpeedInsights />
         <GoogleAnalytics
           gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ""}
         />
